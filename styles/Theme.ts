28daf1f38bbe3294c0b30d160e83d9534d932bc2
juchanhwang <ghkdwncks96@gmail.com>
2022-04-07 16:12:22 +0900

    feat(package.json): remove styled-component, replace to emotion styled library

--- styles/Theme.ts
+++ styles/Theme.ts
@@ -1,3 +1,18 @@
-const theme = {};
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#ff9a67',
+      main: '#ff5600',
+      dark: '#E92A00',
+    },
+    secondary: {
+      light: '#93CCFF',
+      main: '#2897FB',
+      dark: '#0369E0',
+    },
+  },
+});
 
 export default theme;

025a0230edc77da55397290cebdf7d32d065ce07
juchanhwang <ghkdwncks96@gmail.com>
2022-04-26 13:29:38 +0900

    refactor(hooks/useIsLoginStatus): wrap function with useCallback hook

--- src/hooks/useIsLoginStatus.ts
+++ src/hooks/useIsLoginStatus.ts
@@ -1,17 +1,20 @@
 import { ACCESS_TOKEN, REFRESH_TOKEN } from '@common/constant';
 import { useRecoilState } from 'recoil';
 import { isLoginState } from '@common/recoil';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { localStore } from '@common/storage';
 
-export function useIsLoginStatus(): boolean {
-  const [ isLogin, setIsLogin ] = useRecoilState(isLoginState);
+export default useCallback(
+  function useIsLoginStatus(): boolean {
+    const [ isLogin, setIsLogin ] = useRecoilState(isLoginState);
 
-  useEffect(() => {
-    const at = localStore.getItem(ACCESS_TOKEN);
-    const rt = localStore.getItem(REFRESH_TOKEN);
-    setIsLogin(!!at && !!rt);
-  }, [ isLogin ]);
+    useEffect(() => {
+      const at = localStore.getItem(ACCESS_TOKEN);
+      const rt = localStore.getItem(REFRESH_TOKEN);
+      setIsLogin(!!at && !!rt);
+    }, [ isLogin ]);
 
-  return isLogin;
-}
+    return isLogin;
+  },
+  [],
+);

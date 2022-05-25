b4d2b44e5a37d3f58054256269bf0baeabf78739
juchanhwang <ghkdwncks96@gmail.com>
2022-05-25 16:06:29 +0900

    feat(styles/mixins): add mixin file

---
+++ styles/mixins.ts
@@ -0,0 +1,17 @@
+import { css } from '@emotion/css';
+
+export const setLineClamp = (line: number) => (
+  css`
+    word-wrap: break-word;
+    -webkit-line-clamp: ${line};
+    text-overflow: ellipsis;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    // IE 브라우저일 때는 max-height를 줌
+    @media screen and (-ms-high-contrast: active), (-ms-high-contrast: none) {
+      max-height: 19px * ${line};
+    }
+  `
+);
+

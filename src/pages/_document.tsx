077ea829d2923158a344634b8378f36d340b1165
sejung1218 <sejung1218@hanmail.net>
2022-09-01 11:00:12 +0900

    익스플로러 접근 제한

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,12 +4,32 @@ import * as React from 'react';
 
 // const sheets = new ServerStyleSheets();
 // const css = sheets.toString();
+const redirectIEToEdge = () => {
+  const recommendEdgeUrl =
+    'https://support.microsoft.com/office/160fa918-d581-4932-9e4e-1075c4713595';
+  return (
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `if(/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
+        window.location = 'microsoft-edge:' + window.location;
+        setTimeout(function() {
+          window.location = '${recommendEdgeUrl}';
+        }, 1);
+      }`,
+      }}
+    ></script>
+  );
+};
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>{/* <style id="jss-server-side">${css}</style> */}</Head>
+        {/* <Head>{redirectIEToEdge()}</Head> */}
+        <Head>
+          {/* <style id="jss-server-side">${css}</style> */}
+          {redirectIEToEdge()}
+        </Head>
         <body className="loading">
           <Main />
           <NextScript />

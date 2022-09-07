59885cbb0828ec4bc79ecc7e11c1e1e83e805ea2
NewfolderGames <me@newfolder.games>
2022-09-08 00:52:07 +0900

    timer rework

--- src/layouts/Lesson/LessonContentVideo.tsx
+++ src/layouts/Lesson/LessonContentVideo.tsx
@@ -10,7 +10,17 @@ import type {
 } from '@common/api/Api';
 import ApiClient from '@common/api/ApiClient';
 
-const PLAYER_ELEMENT_ID = 'lesson-player' as const;
+const PLAYER_ELEMENT_ID = "lesson-player" as const;
+
+interface RecordProgressArgs {
+  currentCourseUserSeq: number;
+  currentCourseProgress: CourseProgressResponseDto;
+  currentCourseLesson: LessonDetailClientResponseDto;
+  videoCurrentSeconds: number;
+  apiVideoSeconds: number;
+  isEnd: boolean;
+  onSuccess?: () => void;
+}
 
 interface Props {
   coursePlayFirst?: boolean;
@@ -24,7 +34,6 @@ interface Props {
 
 export default function LessonContentVideo(props: Props) {
   const router = useRouter();
-  const routerAsPath = router.asPath;
 
   // 스테이트.
 
@@ -32,15 +41,16 @@ export default function LessonContentVideo(props: Props) {
 
   // 레퍼런스.
 
-  const prevCourseUserSeq = React.useRef<number | null>(null);
-  const prevCourseProgress = React.useRef<CourseProgressResponseDto | null>(null);
-  const prevLesson = React.useRef<LessonDetailClientResponseDto | null>(null);
+  const currentPath = React.useRef<string | null>(null);
 
-  const currentLessonSeq = React.useRef<number | null>(null);
+  const currentCourseUserSeq = React.useRef<number | null>(null);
+  const currentCourseProgress = React.useRef<CourseProgressResponseDto | null>(null);
+  const currentCourseLesson = React.useRef<LessonDetailClientResponseDto | null>(null);
 
   const apiTimer = React.useRef<number | null>(null);
-  const apiSeconds = React.useRef<number>(0);
-  const apiVideoSeconds = React.useRef<number>(0);
+  const apiTimerVideoIncrementTime = React.useRef<() => void | null>(null);
+  const apiTimerVideoSetCurrentSeconds = React.useRef<(time: number) => void | null>(null);
+  const apiTimerSetKill = React.useRef<((mode: "KILL" | "RESTART", isEnd?: boolean) => Promise<void>) | null>(null);
 
   const videoPlayer = React.useRef<Ncplayer | null>(null);
   const vidoeDurationSeconds = React.useRef<number>(0);
@@ -48,8 +58,9 @@ export default function LessonContentVideo(props: Props) {
   const videoPlayedSeconds = React.useRef<number>(0);
   const videoIsSeeking = React.useRef<boolean>(false);
   const videoIsPaused = React.useRef<boolean>(true);
-  const videoIsFirst = React.useRef<boolean>(true);
-  const videoIsFinished = React.useRef<boolean>(false);
+  const videoIsEnded = React.useRef<boolean>(false);
+  const videoIsCompleted = React.useRef<boolean>(false);
+  const videoIsRecording = React.useRef<boolean>(false);
 
   // 콜백
 
@@ -64,233 +75,286 @@ export default function LessonContentVideo(props: Props) {
     );
   }, [props.courseProgress.studyTime, props.lessonCompleted]);
 
-  // 콜백 - 타이머.
+  const recordProgress = React.useCallback((args: RecordProgressArgs) => {
 
-  const stopTimer = React.useCallback(
-    async (mode: 'PREV' | 'CURRENT' | 'RESET', isEnd = false) => {
-      if (apiTimer.current !== null) {
-        window.clearInterval(apiTimer.current);
+    ApiClient.courseLog
+      .createCourseModulesUsingPost1({
+        courseUserSeq: args.currentCourseUserSeq,
+        lessonSeq: args.currentCourseLesson.seq,
+        studyTime: args.apiVideoSeconds,
+      })
+      .then(() =>
+        ApiClient.courseProgress
+          .updateCourseProgressUsingPut({
+            courseUserSeq: args.currentCourseUserSeq,
+            courseProgressSeq: args.currentCourseProgress.courseProgressSeq,
+            lessonSeq: args.currentCourseLesson.seq,
+            studyLastTime: args.videoCurrentSeconds,
+          })
+      )
+      .then((v) => {
 
-        if (mode !== 'RESET') {
-          if (
-            (mode === 'CURRENT' &&
-              (props.lesson === null ||
-                props.courseProgress.courseProgressSeq === null)) ||
-            (mode === 'PREV' &&
-              props.courseUserSeq === prevCourseUserSeq.current &&
-              props.lesson.seq === prevLesson.current.seq)
-          )
-            return;
+        return ApiClient.courseProgress
+          .updateAllCourseProgressUsingPut(args.currentCourseUserSeq)
+          .then(() => {
+            
+            if (v.data.data.completeYn === "Y") {
 
-          if (mode === 'CURRENT') {
-            videoIsFinished.current = true;
-            videoIsFirst.current = true;
-          }
+              props?.onComplete(args?.isEnd);
+              args?.onSuccess();
 
-          const courseUserSeq =
-            mode === 'PREV' ? prevCourseUserSeq.current : props.courseUserSeq;
-          const courseProgressSeq =
-            mode === 'PREV'
-              ? prevCourseProgress.current.courseProgressSeq
-              : props.courseProgress.courseProgressSeq;
-          const lessonSeq = mode === 'PREV' ? prevLesson.current.seq : props.lesson.seq;
-          const currentSecond = videoCurrentSeconds.current;
-
-          await ApiClient.courseLog
-            .createCourseModulesUsingPost1({
-              courseUserSeq: courseUserSeq,
-              lessonSeq: lessonSeq,
-              studyTime: apiVideoSeconds.current,
-            })
-            .then(() =>
-              ApiClient.courseProgress.updateCourseProgressUsingPut({
-                courseUserSeq: courseUserSeq,
-                courseProgressSeq: courseProgressSeq,
-                lessonSeq: lessonSeq,
-                studyLastTime: currentSecond,
-              })
-          )
-          .then((v) => {
-
-            return ApiClient.courseProgress
-              .updateAllCourseProgressUsingPut(courseUserSeq)
-              .then(() => v.data.data.completeYn === "Y" && props.onComplete(isEnd));
+            }
             
           });
+        
+      });
+
+  }, [props])
+
+  // 콜백 - 타이머.
+
+  const startTimer = React.useCallback(() => {
+
+    const courseUserSeq = currentCourseUserSeq.current;
+    const courseProgress = currentCourseProgress.current;
+    const courseLesson = currentCourseLesson.current;
+    const coursePath = currentPath.current;
+    
+    if (courseLesson === null || courseProgress === null) return;
+    
+    ApiClient.courseLog
+      .createCourseModulesUsingPost1({
+        courseUserSeq: currentCourseUserSeq.current,
+        lessonSeq: currentCourseLesson.current.seq,
+        studyTime: 0,
+      });
+
+    let apiTimerKill: "KILL" | "RESTART" | null = null;
+    let apiSeconds = 0;
+    let apiVideoSeconds = 0;
+    let apiVideoCurrentSeconds = videoCurrentSeconds.current;
+    let isVideoEnd = false;
+    
+    let killEvent: () => void;
+
+    apiTimerVideoIncrementTime.current = () => { apiVideoSeconds++; };
+    apiTimerVideoSetCurrentSeconds.current = (time: number) => { apiVideoCurrentSeconds = time; }
+    apiTimerSetKill.current = (mode: "KILL" | "RESTART", isEnd = false) => {
+      
+      apiTimerKill = mode;
+      isVideoEnd = isEnd;
+      return new Promise<void>((res) => { killEvent = res });
+    
+    };
+
+    const timer = window.setInterval(() => {
+
+      console.log(apiTimerKill, currentCourseUserSeq.current, courseUserSeq, currentCourseLesson.current.seq, courseLesson.seq, router.asPath, coursePath);
+      apiSeconds++;
+
+      if (
+        apiTimerKill !== null ||
+        currentCourseUserSeq.current !== courseUserSeq ||
+        currentCourseLesson.current.seq !== courseLesson.seq ||
+        router.asPath !== coursePath ||
+        apiTimer.current !== timer
+      ) {
+
+        if (apiTimerKill === "KILL") {
+
+          window.clearInterval(timer);
+
+          if (apiTimer.current === timer) {
+            
+            apiTimer.current = null;
+            apiTimerVideoIncrementTime.current = null;
+            apiTimerVideoSetCurrentSeconds.current = null;
+            apiTimerSetKill.current = null;
+  
+          }
+
+        }
+        
+        return recordProgress({
+          currentCourseUserSeq: courseUserSeq,
+          currentCourseProgress: courseProgress,
+          currentCourseLesson: courseLesson,
+          apiVideoSeconds: apiSeconds,
+          videoCurrentSeconds: apiVideoCurrentSeconds,
+          isEnd: isVideoEnd,
+          onSuccess: () => {
+            
+            killEvent();
+            if (apiTimerKill === "RESTART") {
+
+              ApiClient.courseLog
+                .createCourseModulesUsingPost1({
+                  courseUserSeq: currentCourseUserSeq.current,
+                  lessonSeq: currentCourseLesson.current.seq,
+                  studyTime: 0,
+                })
+                .then(() => {
+  
+                  apiTimerKill = null;
+                  apiSeconds = 0;
+                  apiVideoSeconds = 0;
+  
+                })
+                .catch(() => {
+
+                  window.clearInterval(timer);
+
+                  if (apiTimer.current === timer) {
+                    
+                    apiTimer.current = null;
+                    apiTimerVideoIncrementTime.current = null;
+                    apiTimerVideoSetCurrentSeconds.current = null;
+                    apiTimerSetKill.current = null;
+          
+                  }
+
+                });
+
+            }
+
+          },
+        });
 
       }
 
-    }
-
-      apiTimer.current = null;
-      apiSeconds.current = 0;
-      apiVideoSeconds.current = 0;
-    },
-    [props]
-  );
-
-  const startTimer = React.useCallback(() => {
-    stopTimer('RESET');
-
-    if (props.lesson === null || props.courseProgress.courseProgressSeq === null) return;
-
-    const courseUserSeq = props.courseUserSeq;
-    const courseProgressSeq = props.courseProgress.courseProgressSeq;
-    const lessonSeq = props.lesson.seq;
-
-    const timer = window.setInterval(() => {
-      if (currentLessonSeq.current !== lessonSeq || router.asPath !== routerAsPath || apiTimer.current !== timer)
-        return clearInterval(timer);
-
-      apiSeconds.current++;
-
-      if (apiSeconds.current >= 5) {
+      if (apiSeconds >= 5 && apiTimerKill === null) {
+        
         ApiClient.courseLog
           .createCourseModulesUsingPost1({
             courseUserSeq: courseUserSeq,
-            lessonSeq: lessonSeq,
-            studyTime: apiVideoSeconds.current,
+            lessonSeq: courseLesson.seq,
+            studyTime: apiVideoSeconds,
           })
           .then(() =>
-            ApiClient.courseProgress.updateCourseProgressUsingPut({
-              courseUserSeq: courseUserSeq,
-              courseProgressSeq: courseProgressSeq,
-              lessonSeq: lessonSeq,
-              studyLastTime: videoCurrentSeconds.current,
-            })
+            ApiClient.courseProgress
+              .updateCourseProgressUsingPut({
+                courseUserSeq: courseUserSeq,
+                courseProgressSeq: courseProgress.courseProgressSeq,
+                lessonSeq: courseLesson.seq,
+                studyLastTime: apiVideoCurrentSeconds,
+              })
           );
 
-        apiSeconds.current = 0;
-        apiVideoSeconds.current = 0;
+        apiSeconds = 0;
+        apiVideoSeconds = 0;
+
       }
+
     }, 1000);
 
     apiTimer.current = timer;
-  }, [
-    props.courseProgress.courseProgressSeq,
-    props.courseUserSeq,
-    props.lesson,
-    router,
-    routerAsPath,
-    stopTimer,
-  ]);
+
+  }, [recordProgress, router.asPath]);
 
   // 콜백 - 이벤트.
 
   const onPause = React.useCallback(() => {
+
     videoIsPaused.current = true;
+
   }, []);
 
   const onPlaying = React.useCallback(() => {
-    if (props.lesson === null || props.courseProgress.courseProgressSeq === null) return;
 
-    if (videoIsFirst.current) {
-      ApiClient.courseLog.createCourseModulesUsingPost1({
-        courseUserSeq: props.courseUserSeq,
-        lessonSeq: props.lesson.seq,
-        studyTime: 0,
-      });
+    if (currentCourseLesson === null || currentCourseProgress === null) return;
+    if (apiTimer.current === null || videoIsEnded.current) {
 
-      videoIsFirst.current = false;
-
+      apiTimerSetKill?.current("KILL");
       startTimer();
+      videoIsEnded.current = false;
+
     }
-
+    
     videoIsPaused.current = false;
-  }, [
-    props.courseProgress.courseProgressSeq,
-    props.courseUserSeq,
-    props.lesson,
-    startTimer,
-  ]);
 
-  const onSeeking = React.useCallback(() => {
-    videoIsSeeking.current = true;
-  }, []);
+  }, [startTimer]);
 
-  const onSeeked = React.useCallback(() => {
-    videoIsSeeking.current = false;
-  }, []);
+  const onSeeking = React.useCallback(() => videoIsSeeking.current = true, []);
 
-  const onTimeChange = React.useCallback(
-    (time: number) => {
-      if (time === videoCurrentSeconds.current) return;
-      if (
-        time !== videoCurrentSeconds.current + 1 ||
-        videoIsPaused.current ||
-        videoIsSeeking.current
-      ) {
-        videoCurrentSeconds.current = time;
-        return;
-      }
+  const onSeeked = React.useCallback(() => videoIsSeeking.current = false, []);
+
+  const onTimeChange = React.useCallback((time: number) => {
+
+    if (time === videoCurrentSeconds.current) return;
+    if (
+      time !== videoCurrentSeconds.current + 1 ||
+      videoIsPaused.current ||
+      videoIsSeeking.current
+    ) {
 
       videoCurrentSeconds.current = time;
-      videoPlayedSeconds.current++;
-      apiVideoSeconds.current++;
+      apiTimerVideoSetCurrentSeconds?.current(time);
+      return;
 
-      if (
-        !props.lessonCompleted &&
-        !videoIsFinished.current &&
-        (
-          props.courseProgress.studyTime + videoPlayedSeconds.current > vidoeDurationSeconds.current ||
-          videoCurrentSeconds.current > vidoeDurationSeconds.current
-        )
-      ) {
+    }
 
-        videoIsFinished.current = true;
-        stopTimer('CURRENT');
+    videoCurrentSeconds.current = time;
+    videoPlayedSeconds.current++;
+    apiTimerVideoIncrementTime?.current();
+    apiTimerVideoSetCurrentSeconds?.current(time);
 
-      }
+    if (!videoIsCompleted.current && !videoIsRecording.current && props.courseProgress.studyTime + videoPlayedSeconds.current >= vidoeDurationSeconds.current) {
+      
+      videoIsRecording.current = true;
+      apiTimerSetKill?.current("RESTART");
+      
+    }
+    
+    updateProgress();
 
-      updateProgress();
-    },
-    [props.courseProgress.studyTime, props.lessonCompleted, stopTimer, updateProgress]
-  );
+  }, [props.courseProgress.studyTime, updateProgress]);
 
   const onEnded = React.useCallback(() => {
+
     videoCurrentSeconds.current = vidoeDurationSeconds.current;
-    videoIsFinished.current = true;
-    videoIsFirst.current = true;
-    stopTimer('CURRENT', true);
-  }, [stopTimer]);
+    videoIsEnded.current = true;
+    apiTimer.current = null;
+    apiTimerSetKill?.current("KILL", true);
+
+  }, []);
 
   // 이펙트.
 
   React.useEffect(() => {
+
     if (!props.coursePlayFirst && videoPlayer.current) videoPlayer.current.play();
+
   }, [props.coursePlayFirst]);
 
   React.useEffect(() => {
-    if (prevCourseUserSeq.current === null)
-      prevCourseUserSeq.current = props.courseUserSeq;
-    if (prevCourseProgress.current === null)
-      prevCourseProgress.current = props.courseProgress;
-    if (prevLesson.current === null) prevLesson.current = props.lesson;
 
-    stopTimer('PREV');
+    apiTimerSetKill?.current("KILL");
+
+    currentPath.current = router.asPath;
+
+    currentCourseUserSeq.current = props.courseUserSeq;
+    currentCourseProgress.current = props.courseProgress;
+    currentCourseLesson.current = props.lesson;
 
     vidoeDurationSeconds.current = props.lesson ? props.lesson.totalTime : 0;
     videoCurrentSeconds.current = props.lesson ? props.courseProgress.studyLastTime : 0;
     videoPlayedSeconds.current = 0;
     videoIsSeeking.current = false;
     videoIsPaused.current = true;
-    videoIsFirst.current = true;
-    videoIsFinished.current = false;
-
-    prevCourseUserSeq.current = props.courseUserSeq;
-    prevCourseProgress.current = props.courseProgress;
-    prevLesson.current = props.lesson;
-
-    currentLessonSeq.current = props.lesson.seq;
-
+    videoIsEnded.current = false;
+    videoIsCompleted.current = false;
+    videoIsRecording.current = false;
+    
     updateProgress();
-  }, [
-    props.lesson,
-    props.courseProgress,
-    props.courseUserSeq,
-    stopTimer,
-    updateProgress,
-  ]);
+
+  }, [props.lesson, props.courseProgress, props.courseUserSeq, updateProgress, router.asPath]);
+
+  React.useEffect(() => {
+
+    videoIsCompleted.current = !!props.lessonCompleted;
+    videoIsRecording.current = false;
+
+  }, [props.lessonCompleted]);
 
   // 렌더링.
 

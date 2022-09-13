848080287fdcf7a880447ef7713e51b33efb1669
cube-00y <lomi1241@gmail.com>
2022-09-13 22:37:24 +0900

    feat: 교육영상 레이아웃 개발

--- src/layouts/Traffic/LearningMaterial/Video/index.tsx
+++ src/layouts/Traffic/LearningMaterial/Video/index.tsx
@@ -1,9 +1,82 @@
-import { MaterialType } from "@common/api/learningMaterial";
+import {
+  VideoItemContentWrapper,
+  VideoItemHeaderDateText,
+  VideoItemHeaderDateWrapper,
+  VideoItemHeaderWrapper,
+  VideoItemWrapper,
+  VideoWrapper,
+} from "@layouts/Traffic/LearningMaterial/Video/style";
+import React, { useState } from "react";
+import {
+  MaterialSubType,
+  MaterialType,
+  useGetLearningMaterial,
+} from "@common/api/learningMaterial";
+import {
+  TableHeader,
+  TableItem,
+  TableWrapper,
+} from "@layouts/Traffic/LearningMaterial/style";
+import ArrowDown from "public/assets/images/ic_arrow_down.svg";
 
 interface VideoLayoutProps {
   materialType: MaterialType;
 }
 
 export default function VideoLayout({ materialType }: VideoLayoutProps) {
-  return <p>Video</p>;
+  const [tabType, setTabType] = useState<MaterialSubType | "">("");
+  const { data, mutate } = useGetLearningMaterial(materialType, tabType);
+
+  const [selectedPost, setSelectedPost] = useState<number | null>(null);
+
+  const handleClickChip = (value: MaterialSubType) => {
+    if (value === tabType) {
+      setTabType("");
+      return;
+    }
+    setTabType(value);
+  };
+
+  const handleClickPost = (id: number) => {
+    if (id === selectedPost) {
+      setSelectedPost(null);
+      return;
+    }
+    setSelectedPost(id);
+  };
+
+  return (
+    <VideoWrapper>
+      <TableWrapper>
+        <TableHeader>
+          <TableItem width="10%">번호</TableItem>
+          <TableItem width="65%">제목</TableItem>
+          <TableItem width="25%">등록일</TableItem>
+        </TableHeader>
+      </TableWrapper>
+
+      {new Array(5).fill(null).map((_, index) => (
+        <VideoItemWrapper
+          open={selectedPost === index}
+          key={index}
+          onClick={() => handleClickPost(index)}
+        >
+          <VideoItemHeaderWrapper>
+            <TableItem width="10%">{index}</TableItem>
+            <TableItem width="65%" textAlign="left">
+              이것은 교육영상 제목입니다.이것은 교육영상 제목입니다.이것은
+              교육영상 제목입니다.
+            </TableItem>
+            <TableItem width="25%">
+              <VideoItemHeaderDateWrapper open={selectedPost === index}>
+                <VideoItemHeaderDateText>2022. 05. 23</VideoItemHeaderDateText>
+                <ArrowDown />
+              </VideoItemHeaderDateWrapper>
+            </TableItem>
+          </VideoItemHeaderWrapper>
+          <VideoItemContentWrapper>asdasd</VideoItemContentWrapper>
+        </VideoItemWrapper>
+      ))}
+    </VideoWrapper>
+  );
 }
---
+++ src/layouts/Traffic/LearningMaterial/Video/style.tsx
@@ -0,0 +1,47 @@
+import styled from "@emotion/styled";
+import { Chip } from "@mui/material";
+import { TableHeader } from "@layouts/Traffic/LearningMaterial/style";
+
+export const VideoWrapper = styled.div`
+  padding: 0 0 230px 0;
+`;
+
+interface VideoItemWrapperProps {
+  open: boolean;
+}
+
+export const VideoItemWrapper = styled.div<VideoItemWrapperProps>`
+  width: 100%;
+  height: ${({ open }) => (open ? `auto` : `75px`)};
+  overflow: hidden;
+  border-bottom: 1px solid #cdcdcd;
+`;
+
+export const VideoItemHeaderWrapper = styled(TableHeader)`
+  border: 0;
+`;
+
+interface VideoItemHeaderDateWrapperProps {
+  open?: boolean;
+}
+
+export const VideoItemHeaderDateWrapper = styled.div<VideoItemHeaderDateWrapperProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+
+  > svg {
+    transform: rotate(${({ open }) => (open ? 0 : "180deg")});
+  }
+`;
+
+export const VideoItemHeaderDateText = styled.p`
+  color: #8f8f8f;
+  margin-right: 12px;
+`;
+
+export const VideoItemContentWrapper = styled.div`
+  padding: 14px 10%;
+  background-color: #fbfbfb;
+`;

313f1145182089cc94be035f121d6a6b9be62bc4
Mellang <51808985+CaraMellang@users.noreply.github.com>
2022-09-23 14:54:00 +0900

    공사중 페이징

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,19 +7,44 @@ import { AppProps } from 'next/app';
 import { ManagedUIContext } from '@components/ui';
 import { RecoilRoot } from 'recoil';
 import { ReactNode } from 'react';
+import dynamic from 'next/dynamic';
+import Image from 'next/image';
 
 const Noop = ({ children }: { children: ReactNode }) => <>{children}</>;
 
 export default function App({ Component, pageProps }: AppProps) {
   const Layout = (Component as any).Layout || Noop;
 
+  const Help = dynamic(
+    () =>
+      Promise.resolve(() => {
+        if (typeof window !== 'undefined') {
+          if (localStorage.getItem('site_admin_type') === 'Y'){
+            return (
+            <Layout pageProps={pageProps}>
+                <Component {...pageProps} />
+            </Layout>
+            );
+            }
+          else {
+            return <div style={{position:'relative', width:'100vw' , height:'100vh' , background:'#06062c'}} >
+              <Image src={'https://kr.object.gov-ncloudstorage.com/cn-lms-storage/resource/underconstruction.png'} layout='fill' />
+              </div>
+          }
+        }
+      }),
+    { ssr: false }
+  );
+
   return (
     <RecoilRoot>
       <ManagedUIContext>
-        <Layout pageProps={pageProps}>
+        <Help/>
+        {/* <Layout pageProps={pageProps}>
           <Component {...pageProps} />
-        </Layout>
+        </Layout> */}
       </ManagedUIContext>
     </RecoilRoot>
   );
 }
+

a509b26a20dd5e67c0aff847835f85c72522f65b
sejung1218 <sejung1218@hanmail.net>
2022-11-28 15:36:51 +0900

    학습자료 UI 변경 및 type 한글 필터 적용

--- src/layouts/AdminCenter/LearningMaterialManagement/LearningMaterialManagement.tsx
+++ src/layouts/AdminCenter/LearningMaterialManagement/LearningMaterialManagement.tsx
@@ -1,12 +1,13 @@
 import {
   learningMaterialList,
   learningMaterialRemove,
-} from '@common/api/learningMaterial';
-import { useDialog } from '@hooks/useDialog';
-import { useSnackbar } from '@hooks/useSnackbar';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+} from "@common/api/learningMaterial";
+import { useDialog } from "@hooks/useDialog";
+import { useSnackbar } from "@hooks/useSnackbar";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import {
+  Box,
   Button,
   Chip,
   Container,
@@ -20,40 +21,40 @@ import {
   TableHead,
   TableRow,
   Typography,
-} from '@mui/material';
-import { Spinner, Table } from '@components/ui';
-import dateFormat from 'dateformat';
-import styled from '@emotion/styled';
-import { ProductStatus } from '@common/api/course';
+} from "@mui/material";
+import { Spinner, Table } from "@components/ui";
+import dateFormat from "dateformat";
+import styled from "@emotion/styled";
+import { ProductStatus } from "@common/api/course";
 
-const headRows = [
-  { name: '번호' },
-  { name: '제목' },
-  { name: '본문' },
-  { name: '타입' },
-  { name: '서브타입' },
-  { name: '생성날짜' },
-  { name: '수정날짜' },
-  { name: 'Url' },
-  { name: '첨부파일' },
-  { name: '상태' },
-  { name: '수정' },
-  // { name: '삭제' },
+const headRows: {
+  name: string;
+  align: "inherit" | "left" | "center" | "right" | "justify";
+  width: string;
+}[] = [
+  // { name: 'No1', align: 'center', width: '2.5%' }, // seq
+  { name: "No", align: "center", width: "5%" }, // seq
+  { name: "학습자료유형", align: "center", width: "8.5%" }, // materialType
+  { name: "학습자료상세유형", align: "center", width: "8.5%" }, // materialSubType
+  { name: "제목", align: "center", width: "32%" }, // title
+  { name: "작성일", align: "center", width: "12%" }, // createdDtime
+  { name: "수정일", align: "center", width: "12%" }, // modifiedDtime
+  { name: "상태", align: "center", width: "5%" }, // status
 ];
 
 const typeTabsConfig = [
-  { name: '전체', value: '' },
-  { name: '연령별 교수학습 지도안', value: 'TYPE_BY_AGE' },
-  { name: '교육자료', value: 'TYPE_EDUCATIONAL' },
-  { name: '교육영상', value: 'TYPE_VIDEO ' },
-  { name: '타기관자료모음', value: 'TYPE_OTHER_ORGAN ' },
+  { name: "전체", value: "" },
+  { name: "연령별 교수학습 지도안", value: "TYPE_BY_AGE" },
+  { name: "교육자료", value: "TYPE_EDUCATIONAL" },
+  { name: "교육영상", value: "TYPE_VIDEO" },
+  { name: "타기관자료모음", value: "TYPE_OTHER_ORGAN" },
 ];
 
 const subTypeTabsConfig = [
-  { name: '어린이', value: 'TYPE_CHILDREN' },
-  { name: '청소년', value: 'TYPE_TEENAGER' },
-  { name: '어르신', value: 'TYPE_ELDERLY ' },
-  { name: '자가운전자', value: 'TYPE_SELF_DRIVING ' },
+  { name: "어린이", value: "TYPE_CHILDREN" },
+  { name: "청소년", value: "TYPE_TEENAGER" },
+  { name: "어르신", value: "TYPE_ELDERLY" },
+  { name: "자가운전자", value: "TYPE_SELF_DRIVING" },
 ];
 
 export function LearningMaterialManagement() {
@@ -62,8 +63,8 @@ export function LearningMaterialManagement() {
   const dialog = useDialog();
   const [page, setPage] = useState(0);
   const [seq, setSeq] = useState<number | null>(null);
-  const [typeValue, setTypeValue] = useState('');
-  const [subTypeValue, setSubTypeValue] = useState('');
+  const [typeValue, setTypeValue] = useState("");
+  const [subTypeValue, setSubTypeValue] = useState("");
   const [subType, setSubType] = useState<boolean>(true);
 
   const { data, error, mutate } = learningMaterialList({
@@ -113,24 +114,33 @@ export function LearningMaterialManagement() {
     });
   };
 
+  console.log("학습자료 data : ", data);
+
   if (error) return <div>...ERROR</div>;
   if (!data) return <Spinner />;
 
   return (
-    <div>
-      <RadioGroup row>
-        {typeTabsConfig.map(({ name, value }: { name: string; value: string }) => (
-          <FormControlLabel
-            key={name}
-            label={name}
-            value={value}
-            control={<Radio checked={typeValue == value} />}
-            onClick={() => setTypeValue(value)}
-          />
-        ))}
+    <Box>
+      <Typography fontSize={30} fontWeight="bold">
+        학습자료 구분
+      </Typography>
+      <RadioGroup row sx={{ mb: 6 }}>
+        {typeTabsConfig.map(
+          ({ name, value }: { name: string; value: string }) => (
+            <FormControlLabel
+              key={name}
+              label={name}
+              value={value}
+              control={<Radio checked={typeValue == value} />}
+              onClick={() => setTypeValue(value)}
+            />
+          )
+        )}
       </RadioGroup>
 
-      <Typography variant="h5">학습자료 목록</Typography>
+      <LearningMaterialTypography variant="h5">
+        학습자료 목록
+      </LearningMaterialTypography>
 
       <Table
         pagination={true}
@@ -138,48 +148,84 @@ export function LearningMaterialManagement() {
         page={data?.number}
         onChangePage={onChangePage}
         size="small"
+        sx={{ tableLayout: "fixed" }}
       >
         <TableHead>
           <TableRow>
-            {headRows.map(({ name }: { name: string }) => (
-              <TableCell key={name} align="center">
-                {name}
-              </TableCell>
-            ))}
+            {headRows.map(
+              ({
+                name,
+                align,
+                width,
+              }: {
+                name: string;
+                align: string;
+                width: string;
+              }) => (
+                <LearningMaterialTitleTableCell
+                  key={name}
+                  align="center"
+                  width={width}
+                >
+                  {name}
+                </LearningMaterialTitleTableCell>
+              )
+            )}
           </TableRow>
         </TableHead>
 
         <TableBody>
-          {data?.content.map(lm => (
-            <TableRow key={lm.seq} hover>
-              <TableCell align="center">{lm.seq}</TableCell>
-              <TableCell align="center">
-                <SubjectTypography>{lm.title}</SubjectTypography>
-              </TableCell>
-              <TableCell align="center">
-                <ContentTypography>{lm.content}</ContentTypography>
-              </TableCell>
-              <TableCell align="center">{lm.materialType}</TableCell>
-              <TableCell align="center">{lm.materialSubType}</TableCell>
-              <TableCell align="center">
-                {dateFormat(lm.createdDtime, 'isoDate')}
-              </TableCell>
-              <TableCell align="center">
-                {dateFormat(lm.modifiedDtime, 'isoDate')}
-              </TableCell>
-              <TableCell align="center">{lm.origin}</TableCell>
-              <TableCell align="center">
-                {lm.s3Files[0] ? lm.s3Files[0].name : '파일없음'}
-              </TableCell>
-              <TableCell style={{ width: 10 }} align="right">
+          {data?.content.map((lm) => (
+            <TableRow
+              sx={{ cursor: "pointer" }}
+              key={lm.seq}
+              hover
+              onClick={() => onClickModifyLM(lm.seq)}
+            >
+              <LearningMaterialTableCell align="center">
+                {lm.seq}
+              </LearningMaterialTableCell>
+              <LearningMaterialTableCell align="center">
+                {
+                  typeTabsConfig.filter(
+                    (item) => item.value === lm.materialType
+                  )[0]?.name
+                }
+                {/* {lm.materialType} */}
+              </LearningMaterialTableCell>
+              <LearningMaterialTableCell align="center">
+                {
+                  subTypeTabsConfig.filter(
+                    (item) => item.value === lm.materialSubType
+                  )[0]?.name
+                }
+              </LearningMaterialTableCell>
+              <LearningMaterialTableCell align="center">
+                <SubjectBox>{lm.title}</SubjectBox>
+              </LearningMaterialTableCell>
+              <LearningMaterialTableCell align="center">
+                {dateFormat(lm.createdDtime, "isoDate")}
+              </LearningMaterialTableCell>
+              <LearningMaterialTableCell align="center">
+                {dateFormat(lm.modifiedDtime, "isoDate")}
+              </LearningMaterialTableCell>
+              {/* <TableCell align="center">{lm.origin}</TableCell> */}
+              {/* <TableCell align="center">
+                {lm.s3Files[0] ? lm.s3Files[0].name : "파일없음"}
+              </TableCell> */}
+              <LearningMaterialTableCell style={{ width: 10 }} align="right">
                 <Chip
                   variant="outlined"
                   size="small"
-                  label={lm.status === ProductStatus.APPROVE ? '정상' : '중지'}
-                  color={lm.status === ProductStatus.APPROVE ? 'secondary' : 'default'}
+                  label={lm.status === ProductStatus.APPROVE ? "정상" : "중지"}
+                  color={
+                    lm.status === ProductStatus.APPROVE
+                      ? "secondary"
+                      : "default"
+                  }
                 />
-              </TableCell>
-              <TableCell align="center">
+              </LearningMaterialTableCell>
+              {/* <TableCell align="center">
                 <Button
                   variant="text"
                   color="warning"
@@ -189,7 +235,7 @@ export function LearningMaterialManagement() {
                 >
                   수정
                 </Button>
-              </TableCell>
+              </TableCell> */}
               {/* <TableCell align="center">
                 <Button
                   variant="text"
@@ -204,20 +250,56 @@ export function LearningMaterialManagement() {
           ))}
         </TableBody>
       </Table>
-    </div>
+    </Box>
   );
 }
 
-const SubjectTypography = styled(Typography)`
-  text-overflow: ellipsis;
-  overflow: hidden;
-  white-space: nowrap;
-  width: 150px;
+// const SubjectTypography = styled(Typography)`
+//   text-overflow: ellipsis;
+//   overflow: hidden;
+//   white-space: nowrap;
+//   width: 150px;
+// `;
+
+// const ContentTypography = styled(Typography)`
+//   text-overflow: ellipsis;
+//   overflow: hidden;
+//   white-space: nowrap;
+//   width: 255px;
+// `;
+
+// 학습자료 목록 글자
+const LearningMaterialTypography = styled(Typography)`
+  margin-bottom: 30px;
+  font-weight: 700;
 `;
 
-const ContentTypography = styled(Typography)`
+// 학습자료 제목. ellipsis 적용.
+const SubjectBox = styled(Box)`
   text-overflow: ellipsis;
   overflow: hidden;
   white-space: nowrap;
-  width: 255px;
+  width: 100%;
+`;
+
+// 학습자료 목록 테이블의 Title부분
+const LearningMaterialTitleTableCell = styled(TableCell)`
+  font-weight: bold;
+  background: #f5f5f5;
+  border-right: 1px solid #f0f0f0;
+  border-top: 1px solid #f0f0f0;
+
+  &:last-child {
+    border-right: 1px solid #f0f0f0;
+  }
+`;
+
+// 학습자료 목록 테이블의 본문
+const LearningMaterialTableCell = styled(TableCell)`
+  margin: 0;
+  border-right: 1px solid #f0f0f0;
+
+  &:first-of-type {
+    background: #f5f5f5;
+  }
 `;

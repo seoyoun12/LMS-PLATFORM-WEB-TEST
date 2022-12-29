42deee6297eeb626c740708921a2313a128d9fe4
Mellang <51808985+CaraMellang@users.noreply.github.com>
2022-12-29 11:48:02 +0900

    사용하지 않는 컴포넌트 삭제(7월부터 남아있던 도민 더미파일) , 도민교육신청 교육대상자선택 스텝 추가(반응형) 및 스텝2에서 exception시 버튼 무한로딩 오류 수정

--- src/layouts/Traffic/Steb/Steb1/Steb1.tsx
+++ src/layouts/Traffic/Steb/Steb1/Steb1.tsx
@@ -1,14 +1,94 @@
-import { CNCalendar } from '@layouts/Calendar';
-import { Box, Container, styled } from '@mui/material';
+import { EduTargetMainType } from '@common/api/learningMaterial';
+import { Box, Container, styled, Typography } from '@mui/material';
+import Link from 'next/link';
 import { StebHeader } from '../StebHeader';
 
 export function Steb1() {
   return (
     <Steb1Wrap>
       <StebHeader value={1} />
-      <CNCalendar />
+      <EduTargetCardsWrap>
+        {eduTargetList.map(r => (
+          <EduTargetCardLink
+            href={`/traffic/stebMove/steb2?eduTargetMain=${r.target}`}
+          >
+            <EduTargetCard
+              sx={{ borderBottom: `2px solid ${r.borderBottomColor}` }}
+            >
+              <EduTargetCardImgWrap></EduTargetCardImgWrap>
+              <EduTargetCardTitleWrap>
+                <Typography sx={{ fontWeight: 'bold', fontSize: '18px' }}>
+                  {r.title}
+                </Typography>
+              </EduTargetCardTitleWrap>
+            </EduTargetCard>
+          </EduTargetCardLink>
+        ))}
+      </EduTargetCardsWrap>
     </Steb1Wrap>
   );
 }
 
-const Steb1Wrap = styled(Container)``;
+const Steb1Wrap = styled(Box)``;
+
+const EduTargetCardsWrap = styled(Box)`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 48px;
+  margin: 128px auto;
+  @media (max-width: 1200px) {
+    max-width: 800px;
+  }
+`;
+
+const EduTargetCardLink = styled(Link)``;
+
+const EduTargetCard = styled(Box)`
+  width: 250px;
+  height: 250px;
+  box-shadow: 2px 2px 8px 4px #bbbbbb;
+`;
+
+const EduTargetCardImgWrap = styled(Box)`
+  height: 200px;
+  background-color: #e0e0e0;
+`;
+const EduTargetCardTitleWrap = styled(Box)`
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const eduTargetList: {
+  title: string;
+  borderBottomColor: string;
+  target: EduTargetMainType;
+  imgPath: string;
+}[] = [
+  {
+    title: '어린이',
+    borderBottomColor: '#f1c40f',
+    target: 'TYPE_CHILDREN',
+    imgPath: '',
+  },
+  {
+    title: '청소년',
+    borderBottomColor: '#2980b9',
+    target: 'TYPE_TEENAGER',
+    imgPath: '',
+  },
+  {
+    title: '자가운전자',
+    borderBottomColor: '#27ae60',
+    target: 'TYPE_SELF_DRIVING',
+    imgPath: '',
+  },
+  {
+    title: '어르신',
+    borderBottomColor: '#c0392b',
+    target: 'TYPE_ELDERLY',
+    imgPath: '',
+  },
+];

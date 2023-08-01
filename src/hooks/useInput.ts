073eb4017014ffe88ff548e23eac035e3bbe678c
Tchaikovsky1114 <forwarm5891@gmail.com>
2023-08-01 17:35:20 +0900

    chore: useInput 타입만 지정

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,9 +1,9 @@
-import { useState } from "react"
+import { ChangeEvent, useState } from "react"
 
-export function useInput(initialState?:any){
+export function useInput(initialState?:string | number){
     const [value , setValue] = useState(initialState)
 
-    const onChangeValue = (e:any) => {
+    const onChangeValue = (e:ChangeEvent<HTMLInputElement>) => {
         setValue(e.target.value)
     }
 

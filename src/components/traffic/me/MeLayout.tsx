1a0095f1ab71e953f9944917909a2d4d37ef2f40
seoyoun <znzl971212@naver.com>
2024-01-18 14:09:49 +0900

    nav 오류 수정

--- src/components/traffic/me/MeLayout.tsx
+++ src/components/traffic/me/MeLayout.tsx
@@ -4,7 +4,7 @@ import MeDrawer from '../drawer/MeDrawer'
 import { Footer } from '@components/common'
 import { memo } from 'react'
 import TopNavigation from './common/navigation/TopNavigation'
-
+import { TrafficGlobalNavigationBar } from '@components/common/GlobalNavigationBar/TrafficGlobalNavigationBar'
 interface Props {
   children: React.ReactNode;
   title: string;
@@ -13,7 +13,8 @@ interface Props {
 export const MeLayout = ({children,title}: Props) => {
   return (
     <>
-      <TopNavigation />
+      <TrafficGlobalNavigationBar />
+      {/* <TopNavigation /> */}
         <Wrapper>
           <Title>{title ?? '타이틀을 입력해주세요'}</Title>
           <MeDrawer />
@@ -52,4 +53,61 @@ const Title = styled(Typography)`
   padding-bottom: .25rem;
   border-bottom: 1px solid rgba(151,49,51,0.78);
   text-align: center;
+`;
+
+
+const Header = styled.header`
+  width: 100%;
+  height: 100%;
+`;
+
+// const TitleTypography = styled(Typography)`
+//   /* box-sizing: border-box;
+//   border: 1px solid black; */
+//   color: black;
+//   width: 150px;
+//   font-weight: bold;
+//   font-size: 1.3rem;
+//   white-space: nowrap;
+//   margin-left: 50px;
+// `;
+
+const ContentContainer = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+
+  @media screen and (min-width: 640px) {
+    height: 56px;
+  }
+
+  @media screen and (min-width: 1024px) {
+    height: 78px;
+  }
+`;
+const SignBoxes = styled(Box)`
+  display: flex;
+  justify-content: space-between;
+`;
+
+// const NavContainer = styled.div`
+//   display: flex;
+//   align-content: center;
+//   margin-left: 20px;
+
+//   a:not(:first-of-type) {
+//     margin-left: 16px;
+//   }
+
+//   .bold-600 {
+//     font-weight: 600;
+//   }
+// `;
+
+// const SearchbarContainer = styled.div`
+//   padding: 0 0 0 36px;
+// `;
+
+const RightSection = styled.div`
+  flex-basis: 200px;
 `;
